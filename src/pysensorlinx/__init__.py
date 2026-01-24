bfa9c7b7ec1078a320e1db9351069c08ca62bf2c
Stefan Slivinski <sslivins@gmail.com>
2026-01-24 12:53:25 -0800

    bumped version

--- src/pysensorlinx/__init__.py
+++ src/pysensorlinx/__init__.py
@@ -1,4 +1,4 @@
 from .sensorlinx import Sensorlinx, Temperature, SensorlinxDevice, InvalidCredentialsError, LoginTimeoutError, LoginError, InvalidParameterError
 
 __all__ = ["Sensorlinx", "Temperature", "SensorlinxDevice", "InvalidCredentialsError", "LoginTimeoutError", "LoginError", "InvalidParameterError"]
-__version__ = "0.1.1"
+__version__ = "0.1.2
